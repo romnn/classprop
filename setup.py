b09be40b88b8532bcf2baf35443661c4eaab26ed
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-03-13 16:40:50 +0100

    Remove cli

--- setup.py
+++ setup.py
@@ -71,11 +71,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    entry_points={
-        "console_scripts": [
-            "classprop=classprop.cli:main"
-        ]
-    },
     python_requires=">=3.6",
     install_requires=requirements,
     setup_requires=tool_requirements,
--- tests/test_classprop.py
+++ tests/test_classprop.py
@@ -6,9 +6,8 @@
 import typing
 
 import pytest
-from click.testing import CliRunner
 
-from classprop import classprop, cli
+from classprop import classprop
 
 
 @pytest.fixture  # type: ignore
@@ -27,14 +26,3 @@ def test_content(response: typing.Any) -> None:
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
     pass
-
-
-def test_command_line_interface() -> None:
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "classprop.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
